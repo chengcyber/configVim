dc35ff03715411083cdd349d5002963db8410ea3
kimochg <kimochg@outlook.com>
2017-07-02 12:46:12 +0800

    update install.sh add folders

--- install.sh
+++ install.sh
@@ -11,6 +11,15 @@ for i in $HOME/.vim $HOME/.vimrc; do [ -L $i ] && unlink $i ; done
 echo "Step2: setting up system links"
 lnif $CURRENT_DIR/vimrc $HOME/.vimrc
 lnif "$CURRENT_DIR/.vim" "$HOME/.vim"
+if [[ ! -e $HOME/.vim/tmp/backup ]]; then
+  mkdir -p $HOME/.vim/tmp/backup
+fi
+if [[ ! -e $HOME/.vim/tmp/swap ]]; then
+  mkdir -p $HOME/.vim/tmp/swap
+fi
+if [[ ! -e $HOME/.vim/tmp/undo ]]; then
+  mkdir -p $HOME/.vim/tmp/undo
+fi
 
 echo "Step3: update/install plugins via Vundle"
 vim +PluginInstall!
