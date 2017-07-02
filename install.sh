b5df7ea4d6711e5747ac41c8444d1ac192a42257
kimochg <kimochg@outlook.com>
2017-07-02 12:41:27 +0800

    add install shell

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+CURRENT_DIR=`pwd`
+cd $HOME
+
+echo "Step1: backup current vim config"
+today=`date +%Y%m%d`
+for i in $HOME/.vim $HOME/.vimrc; do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today; done
+for i in $HOME/.vim $HOME/.vimrc; do [ -L $i ] && unlink $i ; done
+
+echo "Step2: setting up system links"
+lnif $CURRENT_DIR/vimrc $HOME/.vimrc
+lnif "$CURRENT_DIR/.vim" "$HOME/.vim"
+
+echo "Step3: update/install plugins via Vundle"
+vim +PluginInstall!
+
+echo "Step4: complie YouCompleteMe"
+echo "if error, goto following link and compile yourself"
+echo "https://github.com/Valloric/YouCompleteMe"
+cd $HOME/.vim/bundle/YouCompleteMe
+if [ `which npm` ]
+then
+  sudo npm install -g typescript
+fi
+python install.py --tern-completer
+
+echo "Done!"
